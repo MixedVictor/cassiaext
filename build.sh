512dc5bfa69fceb4112cdf354dc2053207540c7e
MixedVictor <57690852+MixedVictor@users.noreply.github.com>
2024-06-28 14:57:33 -0300

    Remove exiting if build folder doesn't exist

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ SDK_PATH=/opt/android-sdk
 NDK_VER=26.3.11579264
 NDK_PATH=$SDK_PATH/ndk/$NDK_VER
 
-# In case if build folder doesn't exist (in case if you're running outside the cassiaext folder) exit program
-[[ -d "build" ]] && rm -rf build    || exit 1
+[[ -d "build" ]] && rm -rf build
 clear;cmake -GNinja -Bbuild -H.                                                     \
             -DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake    \
             -DANDROID_PLATFORM=android-29                                           \
